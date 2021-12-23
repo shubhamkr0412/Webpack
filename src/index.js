75570cefa56f56ddad902b3553a42fc26bb63570
shubhamkr0412 <91714750+shubhamkr0412@users.noreply.github.com>
2021-12-24 00:01:14 +0530

    installed react and react-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { add } from "./calc.js";
 import("./index.css");
-console.log(add(5, 3));
+import React from "react";
+import ReactDom from "react-dom";
+console.log(add(50, 3));
 
-const h1 = document.createElement("h1");
-h1.innerText = "Hello Webpack";
-h1.classList.add("redcolor");
-document.getElementById("root").appendChild(h1);
+// const h1 = document.createElement("h1");
+// h1.innerText = "Hello Webpack Let's Start";
+// h1.classList.add("redcolor");
+// document.getElementById("root").appendChild(h1);
+
+ReactDom.render("Hello React", document.getElementById("root"));
