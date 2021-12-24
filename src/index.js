8242804201ea56510f3fc9d2c6bff567e3bb8ce5
shubhamkr0412 <91714750+shubhamkr0412@users.noreply.github.com>
2021-12-24 12:42:34 +0530

    babel

--- src/index.js
+++ src/index.js
@@ -9,4 +9,15 @@ console.log(add(50, 3));
 // h1.classList.add("redcolor");
 // document.getElementById("root").appendChild(h1);
 
-ReactDom.render("Hello React", document.getElementById("root"));
+// ReactDom.render("Hello React", document.getElementById("root"));
+ReactDom.render(
+  // React.createElement("h1", { className: "redcolor" }, [
+  //   "Hello React ",
+
+  //   React.createElement("b", null, "webpack"),
+  // ]),
+  <h1 className="redcolor">
+    {""} Hello WebPack and <i>React</i>{" "}
+  </h1>,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
     ],
   },
 };
